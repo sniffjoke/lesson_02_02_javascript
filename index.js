fda3df9e9d3b171ee5511237913b53e6741680f1
sniffjoke <sniffjoke@gmail.com>
2024-08-23 17:59:06 +0200

    callback hell promise-constructor

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
 // promises .than .catch .finally
 
+// callbackFetch(`https://books-store.com/authors/${data.id}`, (err, data) => {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         callbackFetch(`https://books-store.com/authors/authorId/${data.books}`, (err, data) => {
+//             if (err) {
+//                 console.log(err)
+//             } else {
+//                 callbackFetch(`https://books-store.com/authors/authorId/books/${data.booksId}`, (err, data) => {
+//                     if (err) {
+//                         console.log(err)
+//                     } else {
+//                         callbackFetch(`https://books-store.com/authors/${data.id}`, (err, data) =>
+//                     }
+//                 })
+//             }
+//         })
+//     }
+// })
 
+// promise --> pending, fulfilled, rejected
+
+// const promise = new Promise((resolve, reject) => {})
+
+// function Promise(executor) {
+//     const resolve = (data) => {
+//         return {
+//             state: 'fulfilled',
+//             result: data
+//         }
+//     }
+//     const rejected = (err) => {
+//         return {
+//             state: 'rejected',
+//             error: err
+//         }
+//     }
+//     executor(resolve, rejected)
+// }
+
+// const promise = new Promise((res, rej) => {
+//     setTimeout(() => {
+//         const data = 'hello'
+//         const err = 'something wrong'
+//         res(data)
+//         // rej(err)
+//     }, 2000)
+// })
+//
+// console.log(promise)
+
+const fs = require('fs')
+
+function readFileByPromise(url) {
+    return new Promise((res, rej) => {
+        fs.readFile(url, (err, data) => {
+            if (err) {
+                rej(err)
+            } else {
+                res(data)
+            }
+        })
+    })
+}
+
+export default readFileByPromise
